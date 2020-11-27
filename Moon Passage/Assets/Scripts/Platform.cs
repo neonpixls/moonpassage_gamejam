100890ee32ef5afaee2de8c740df9f404cb4eb08
neonpixls <65367391+neonpixls@users.noreply.github.com>
2020-11-27 16:12:07 -0600

    Added Platform Script

---
+++ Moon Passage/Assets/Scripts/Platform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    [SerializeField] float jumpForce = 10.0f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            //set velocity and set speed of traveling upwards directly
+            Vector2 velocity = rb.velocity;
+            //control de y velocity of the player after it hits platform
+            velocity.y = jumpForce;
+            //set vector back to original
+            rb.velocity = velocity;
+        }
+    }
+}
