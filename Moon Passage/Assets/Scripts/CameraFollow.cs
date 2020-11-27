326b2f05b2f97e42f84499e4b5c26eb55275501f
neonpixls <65367391+neonpixls@users.noreply.github.com>
2020-11-27 16:59:41 -0600

    Added Camera Follow
    camera follows players while it bounces.

---
+++ Moon Passage/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    //variable that references player
+    public Transform target;
+
+    
+    void Update()
+    {
+        //check if the player y value is greater than the camera y value
+        if (target.position.y > transform.position.y)
+        {
+            transform.position = new Vector3 (transform.position.x, target.position.y, transform.position.z);
+        }
+    }
+}
