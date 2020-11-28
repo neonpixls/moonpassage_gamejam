8014d6e1d9a0727e0e11a20ae9b8855c6149f39b
neonpixls <65367391+neonpixls@users.noreply.github.com>
2020-11-28 16:07:10 -0600

    Finished camera follow and added level generator

--- Moon Passage/Assets/Scripts/CameraFollow.cs
+++ Moon Passage/Assets/Scripts/CameraFollow.cs
@@ -6,14 +6,18 @@ public class CameraFollow : MonoBehaviour
 {
     //variable that references player
     public Transform target;
+    public float smoothSpeed = .3f;
+
+    private Vector3 currentVelocity;
 
     
-    void Update()
+    void LateUpdate()
     {
         //check if the player y value is greater than the camera y value
         if (target.position.y > transform.position.y)
         {
-            transform.position = new Vector3 (transform.position.x, target.position.y, transform.position.z);
+            Vector3 newPos = new Vector3(transform.position.x, target.position.y, transform.position.z);
+            transform.position = Vector3.SmoothDamp(transform.position, newPos, ref currentVelocity, smoothSpeed * Time.deltaTime);            
         }
     }
 }
---
+++ Moon Passage/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject platformPrefab;
+
+    public int numberOfPlatforms = 200;
+
+    public float levelWidth = 3f;
+    public float minY = .2f;
+    public float maxY = 1.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 spawnPosition = new Vector3();
+
+        //a loop to specify what number of platforms will be generated and where
+        for (int i = 0; i < numberOfPlatforms;  i++)
+        {
+            //when platforms are created they increase between the two values specified
+            spawnPosition.y += Random.Range(minY, maxY);
+            //create platforms in this position and rotation
+            Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
