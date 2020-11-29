256b1641c56881c14e0203f9a29c5027dd3e9ce4
neonpixls <65367391+neonpixls@users.noreply.github.com>
2020-11-29 12:34:54 -0600

    Oxygen Depletion bar working

---
+++ Moon Passage/Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private int score = 0;
+}
---
+++ Moon Passage/Assets/Scripts/Oxygen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+
+public class Oxygen : MonoBehaviour
+{
+    public Slider oxygenBar;
+    public float oxygenAmount;   
+    private float currentOxygen;
+
+
+    void Start()
+    {
+        currentOxygen = oxygenAmount;        
+    }
+
+    void Update()
+    {
+        currentOxygen -= Time.deltaTime;
+        Debug.Log(currentOxygen);
+        oxygenBar.value = currentOxygen / oxygenAmount;
+    }
+
+
+
+}
