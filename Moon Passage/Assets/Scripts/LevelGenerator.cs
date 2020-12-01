16b7a909ae1681579a9183a94c245e241999ce75
neonpixls <65367391+neonpixls@users.noreply.github.com>
2020-11-30 21:22:33 -0600

    Win Screen image added. Boost platforms added. Win Game finished.

--- Moon Passage/Assets/Scripts/LevelGenerator.cs
+++ Moon Passage/Assets/Scripts/LevelGenerator.cs
@@ -4,33 +4,24 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    [SerializeField] GameObject[] platformPrefab;
+    public GameObject platformsPrefabs;
 
     public int numberOfPlatforms;
 
-    public float levelWidth = 10f;
-    public float minY = .3f;
-    public float maxY = 1.5f;
-
+    public float levelWidth = 5.0f;
+    public float minY = 1.5f;
+    public float maxY = 2.5f;   
     
-            
     void Start()
     {
-        Vector3 spawnPosition = new Vector3();
-        int index = Random.Range(0, platformPrefab.Length);
-        
+        Vector3 spawnPosition = new Vector3();        
 
-        //a loop to specify what number of platforms will be generated and where
-        for (int i = 0; i < numberOfPlatforms;  i++)
+        for (int i = 0; i < numberOfPlatforms; i++)
         {
-            //create platforms in this position and rotation
-            Instantiate(platformPrefab[index], spawnPosition, platformPrefab[index].transform.rotation);
-            //when platforms are created they increase between the two values specified on y
             spawnPosition.y += Random.Range(minY, maxY);
-            //when platforms are created they increase between the two values specified on x
-            spawnPosition.x = Random.Range(-levelWidth, +levelWidth);
-            
+            spawnPosition.x = Random.Range(-levelWidth, levelWidth);
+            Instantiate(platformsPrefabs, spawnPosition, Quaternion.identity);
             
         }
-    }    
+    }
 }
